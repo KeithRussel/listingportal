f3ea5c94380462d8f4fab1d123a84f3488d045ab
KeithRussel <keithmaniacop@gmail.com>
2022-10-07 16:49:33 +0800

    Remove unecessary comment

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,7 +5,13 @@ export default function Home({ posts }) {
     <div>
       {/* loop over the posts and show in UI */}
       {posts &&
-        posts.map((post) => <div key={post.id}>{<h2>{post.Title}</h2>}</div>)}
+        posts.map((post) => (
+          <div key={post.id}>
+            {<h2>{post.Title}</h2>}
+            {<pre>{post.User.username}</pre>}
+          </div>
+        ))}
+      {/* {console.log(posts)} */}
     </div>
   );
 }
@@ -14,7 +20,7 @@ export async function getStaticProps() {
   console.log("this is server side render");
 
   // get posts from our api
-  const res = await fetch("http://localhost:1338/api/posts");
+  const res = await fetch("http://localhost:1338/api/posts?populate=*");
   const results = await res.json();
   // I used normalize to return the data that I need not the attribute object
   const posts = normalize(results);
