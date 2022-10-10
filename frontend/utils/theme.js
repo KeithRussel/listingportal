f729e482a1bc31b6d7d03c796a03cac0b1c9e59d
KeithRussel <keithmaniacop@gmail.com>
2022-10-10 10:07:46 +0800

    Setup Header Component and Theme

---
+++ frontend/utils/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primaryDark: "#112D4E",
+    secondaryDark: "#3F72AF",
+    primaryLight: "#F9F7F7",
+    secondaryLight: "#DBE2EF",
+  },
+  fonts: {
+    primary: "Playfair Display",
+    secondary: "Source Sans Pro",
+  },
+};
