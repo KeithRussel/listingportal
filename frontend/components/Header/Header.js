193a64e3eb14a1a81a6549a26bac10dfb115b3cc
KeithRussel <keithmaniacop@gmail.com>
2022-10-10 21:35:53 +0800

    Header styling class

--- frontend/components/Header/Header.js
+++ frontend/components/Header/Header.js
@@ -3,10 +3,10 @@ import styles from "../Header/Header.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <div className="wrapper">
+    <div className="header">
+      <div className={styles.wrapper}>
         <a href="#">Website Logo Here</a>
-        <nav>
+        <nav className={styles.nav}>
           <ul>
             <li>
               <a href="#">Home</a>
