91849e9821479b7b4e74483112713e34a0039eb7
KeithRussel <keithmaniacop@gmail.com>
2022-10-11 17:37:43 +0800

    Modify Layout Component

--- frontend/components/Layout/Layout.js
+++ frontend/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main>
+        <div className={styles.container}>{children}</div>
+      </main>
     </>
   );
 };
