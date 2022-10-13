84a9f45be88dc730516524788e968f8f0cc953e3
KeithRussel <keithmaniacop@gmail.com>
2022-10-13 09:47:25 +0800

    Add HeroServices Component

---
+++ frontend/components/Hero/HeroServices.js
@@ -0,0 +1,29 @@
+import React from "react";
+import style from "./Hero.module.css";
+import styled from "@emotion/styled";
+
+let Card = styled("div")`
+  & > img {
+    border-radius: 50%;
+  }
+`;
+
+const HeroServices = ({ image, text, subtext }) => {
+  return (
+    <>
+      <Card>
+        <img src={image} />
+        <h3>{text}</h3>
+        <p>{subtext}</p>
+      </Card>
+    </>
+  );
+};
+
+HeroServices.defaultProps = {
+  image: "https://via.placeholder.com/250",
+  text: "sample title",
+  subtext: "Lorem Ipsum dolor imet",
+};
+
+export default HeroServices;
